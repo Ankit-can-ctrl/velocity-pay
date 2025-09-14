57be58533aa75465c3f57469b28457b65f5025b8
Ankit-can-ctrl1 <ak537664@gmail.com>
2025-09-14 22:29:17 +0530

    created user model

---
+++ backend/src/models/User.tsx
@@ -0,0 +1,22 @@
+import mongoose, { Document, model, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
