f5ae0fc002a3f6b0f2c43fc9b44c13de9d6e290b
Ankit-can-ctrl1 <ak537664@gmail.com>
2025-09-30 12:13:08 +0530

    added account schema

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -2,6 +2,15 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
+import Account from "../models/account";
+
+function getRandomBalance(min = 1000, max = 10000) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomSigninBalance(min = 1000, max = 5000) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 export const signup = async (req: Request, res: Response) => {
   const { name, username, email, password } = req.body;
@@ -26,13 +35,19 @@ export const signup = async (req: Request, res: Response) => {
       password: hashedPassword,
     });
 
+    const account = await Account.create({
+      user: user._id,
+      email: user.email,
+      balance: getRandomBalance(),
+    });
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
       expiresIn: "1h",
     });
 
     return res
       .status(201)
-      .json({ token, message: "User created successfully :)" });
+      .json({ token, message: "User account created successfully :)" });
   } catch (error) {
     res
       .status(500)
@@ -52,6 +67,12 @@ export const signin = async (req: Request, res: Response) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid password!" });
+
+    const updatedAccount = await Account.findOneAndUpdate(
+      { user: user._id },
+      { balance: getRandomSigninBalance() },
+      { new: true } //return updated document
+    );
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
       expiresIn: "1h",
---
+++ backend/src/models/account.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId, //reference to user
+      ref: "User",
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Account;
